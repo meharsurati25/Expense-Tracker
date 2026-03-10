3389a8790c6121d8999b0e3c3f52a46b83b7b1ee
harmansinghcse <harman.singh.cse@gmail.com>
2026-03-10 13:01:31 +0530

    dc

---
+++ script.js
@@ -0,0 +1,31 @@
+// Expense list
+let expenses = [];
+
+// get elements from HTML
+const All = document.getElementById("all");
+const Credit = document.getElementById("credit");
+const Debit = document.getElementById("debit");
+const expense_input  = document.getElementById("transaction_input")
+
+// Current filter state
+let currentfilter = "all";
+
+// filter buttons
+All.addEventListener("click", function(){
+    currentfilter = "all";
+    setActiveButton(this);
+})
+
+Credit.addEventListener("click", function(){
+    currentfilter = "credit";
+})
+
+Debit.addEventListener("click", function(){
+    currentfilter = "debit";
+})
+
+
+// Add new transaction
+function add_transaction(){
+    connt
+}
